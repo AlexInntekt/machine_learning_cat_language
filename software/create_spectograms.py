b5bf942bf8e7772ba6f07f0fa95d46f138139803
alexinntekt <alex.inntekt@gmail.com>
2020-05-20 15:16:22 +0300

    sp

--- software/create_spectograms.py
+++ software/create_spectograms.py
@@ -1,5 +1,8 @@
 import os
 
+import pylab
+import wave
+
 import config
 from bujor_library import plotInFrequencyAndSave,readWav
 
@@ -24,14 +27,34 @@ def create_spectograms():
                 save_path = os.path.join(save_path, filename)
                 save_path = save_path+".jpg"
                 save_path = save_path.replace(".wav","")
+
                 print("Converting ")
                 print(input_file_path)
                 print("..to..")
                 print(save_path)
-                # plot_and_save(input_file_path, output_file_path)
                 print("\n\n")
+
+                graph_spectrogram(input_file_path, save_path)
+                
             else:
                 continue
+
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+
+def graph_spectrogram(input_file_path, output_file_path):
+    sound_info, frame_rate = get_wav_info(input_file_path)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    # pylab.title('spectrogram of %r' % input_file_path)
+    pylab.title('spectrogram')
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig(output_file_path)
 
 def plot_and_save(input_file_path, output_file_path):
     fs, x = readWav(input_file_path)
--- software/manage.py
+++ software/manage.py
@@ -5,16 +5,10 @@ from bujor_library import plotInFrequencyAndSave, readWav
 
 
 
-def plot_and_save(file_path):
-    fs, x = readWav(file_path)
-    plotInFrequencyAndSave(x, fs, "rezult.jpg")
-
-    pass
 
 def start():
 
     create_spectograms.create_spectograms()
-    # plot_and_save("/home/alex/Desktop/FILS/NNGA/proj/data/audio_data/growling/41.wav")
 
 start()
 
